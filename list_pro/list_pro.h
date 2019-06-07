126f4762913cd79254ecf11c46ca2a447b975392
hongknight <13750897616@163.com>
2019-06-07 23:36:04 +0800

    add header

--- list_pro/list_pro.h
+++ list_pro/list_pro.h
@@ -14,30 +14,30 @@ typedef struct Node
 } Node;
 
 //链表反转
-Node* list_reverse(Node* list);
+Node* list_reverse(Node* head);
 
 //判断链表是否有环
-bool has_cycle(Node* list);
+bool has_cycle(Node* head);
 
 //查找环形入口
-Node* cycle_entry(Node* list);
+Node* cycle_entry(Node* head);
 
 //查找公共结点
-Node* list_public(Node* list1,Node* list2);
+Node* list_public_1(Node* head1,Node* head2);
 
 //查找公共结点
-Node* list_public(Node* list1,Node* list2);
+Node* list_public(Node* head1,Node* head2);
 
 //合并两个有序链表
-Node* merge_two_list(Node* list1, Node* list2);
+Node* merge_two_list(Node* head1, Node* head2);
 
 //查找倒数第K个结点
-Node* last_k(Node* list, int k);
-
-//删除重复结点
-Node* del_repeat(Node* head);
+Node* last_k(Node* head, int k);
 
 //查找中间结点
 Node* find_middle(Node* head);
+
+//删除重复结点
+Node* del_repeat(Node* head);
 
 #endif//__LIST_PRO_H_
