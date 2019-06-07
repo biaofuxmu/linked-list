d48ca1f66339526a0ce75f3871fdf38a76257144
hongknight <13750897616@163.com>
2019-06-07 23:40:52 +0800

    modify function of finding public node

--- list_pro/list_pro.c
+++ list_pro/list_pro.c
@@ -13,14 +13,14 @@ size_t list_len(Node* head)
 }
 
 //链表反转
-Node* list_reverse(Node* list)
+Node* list_reverse(Node* head)
 {
-    if(list == NULL || list->next == NULL)
+    if(head == NULL || head->next == NULL)
     {
-         return list;
+         return head;
     }
-    Node* p = list;
-    Node* q = list->next;
+    Node* p = head;
+    Node* q = head->next;
     Node* r = q->next;
 
     p->next = NULL;
@@ -39,42 +39,43 @@ Node* list_reverse(Node* list)
 //判断链表是否有环
 //思路：先判断链表的长度是否>1
 //定义两个指针，一个指向头结点slow，一个指向头结点的下一个结点fast，然后slow每次走一步，fast每次走两步，如果有环，则它们将会相遇
-bool has_cycle(Node* list)
+bool has_cycle(Node* head)
 {
-    if(list == NULL || list->next==NULL)
+    if(head == NULL || head->next==NULL)
         return false;
-    Node* slow = list;
-    Node* fast = list->next;
+    Node* slow = head;
+    Node* fast = head;
     while(slow && fast->next)
     {
-        if(slow == fast)
-            return true;
         slow = slow->next;
         fast = fast->next->next;
+        if(slow == fast)
+            return true;
     }
     return false;
 }
 
 //查找环形入口
-Node* cycle_entry(Node* list)
+Node* cycle_entry(Node* head)
 {
-    if(list == NULL || list->next==NULL)
-        return false;
-    Node* slow = list;
-    Node* fast = list->next;
+    if(head == NULL || head->next==NULL)
+        return NULL;
+    Node* slow = head;
+    Node* fast = head;
     Node* meet = NULL;
     while(slow && fast->next)
     {
-        if(slow == fast)
-            break;
         slow = slow->next;
         fast = fast->next->next;
+        if(slow == fast)
+            break;
     }
     if(slow && fast)
     {
-        if(slow == list)
-            return list;
-        slow = list;
+        if(slow == head)
+            return head;
+        slow = head;
+        //fast = fast->next;
         while(slow != fast)
         {
             slow = slow->next;
@@ -86,25 +87,25 @@ Node* cycle_entry(Node* list)
 }
 
 //查找公共结点
-Node* list_public_1(Node* list1,Node* list2)
+Node* list_public_1(Node* head1,Node* head2)
 {
-    if(list1 == NULL || list2 == NULL)
+    if(head1 == NULL || head2 == NULL)
         return NULL;
-    int len1 = list_len(list1);
-    int len2 = list_len(list2);
+    int len1 = list_len(head1);
+    int len2 = list_len(head2);
     int length = 0;
     Node* long_list = NULL;
     Node* short_list = NULL;
     if(len1 > len2)
     {
-        long_list = list1;
-        short_list = list2;
+        long_list = head1;
+        short_list = head2;
         length = len1 - len2;
     }
     else
     {
-        long_list = list2;
-        short_list = list1;
+        long_list = head2;
+        short_list = head1;
         length = len2 - len1;
     }
     for(int i = 0;i < length; i++)
@@ -122,15 +123,15 @@ Node* list_public_1(Node* list1,Node* list2)
 }
 
 //查找公共结点
-Node* list_public(Node* list1,Node* list2)
+Node* list_public(Node* head1,Node* head2)
 {
-    if(list1 == NULL || list2 == NULL)
+    if(head1 == NULL || head2 == NULL)
         return NULL;
-    int len1 = list_len(list1);
-    int len2 = list_len(list2);
+    int len1 = list_len(head1);
+    int len2 = list_len(head2);
 
-    Node* long_list = len1 > len2 ? list1 : list2;
-    Node* short_list = len1 > len2 ? list2 : list1;
+    Node* long_list = len1 > len2 ? head1 : head2;
+    Node* short_list = len1 > len2 ? head2 : head1;
 
     for(int i = 0;i < abs(len1 - len2); i++)
     {
@@ -148,33 +149,33 @@ Node* list_public(Node* list1,Node* list2)
 
 //合并两个有序链表(从小到大排列)
 //先比较头结点 如果list1大，那么list1就是头结点，他下个节点就list1->next和list2的小的哪个节点，递归
-Node* merge_two_list(Node* list1, Node* list2)
+Node* merge_two_list(Node* head1, Node* head2)
 {
-    if(list1==NULL)
-        return list2;
-    if(list2==NULL)
-        return list1;
-    if(list1->data < list2->data)
+    if(head1==NULL)
+        return head2;
+    if(head2==NULL)
+        return head1;
+    if(head1->data < head2->data)
     {
-        list1->next=merge_two_list(list1->next,list2);
-        return list1;
+        head1->next=merge_two_list(head1->next,head2);
+        return head1;
     }
     else
     {
-        list2->next=merge_two_list(list1,list2->next);
-        return list2;
+        head2->next=merge_two_list(head1,head2->next);
+        return head2;
     }
 }
 
 //查找倒数第K个结点
 //两个指针
 //时间复杂度O(n)
-Node* last_k(Node* list, int k)
+Node* last_k(Node* head, int k)
 {
-    if(list == NULL)
+    if(head == NULL)
         return NULL;
-    Node* src = list;
-    Node* dst = list;
+    Node* src = head;
+    Node* dst = head;
     for(int i=0;i<k-1;i++)
     {
         if(src)
@@ -190,7 +191,24 @@ Node* last_k(Node* list, int k)
     return dst;
 }
 
-//删除重复结点
+//查找中间结点, 如果中间结点为两个，返回较小的那个
+//双指针法
+Node* find_middle(Node* head)
+{
+    if(head == NULL || head->next == NULL)
+        return head;
+    Node* slow = head;
+    Node* fast = head;
+    while(fast->next && fast->next->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+
+//删除有序链表重复结点
+//删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。
 Node* del_repeat(Node* head)
 {
     if(head == NULL || head->next==NULL)
@@ -231,18 +249,4 @@ Node* del_repeat(Node* head)
     return head_tmp;
 }
 
-//查找中间结点
-//双指针法
-Node* find_middle(Node* head)
-{
-    if(head == NULL || head->next == NULL)
-        return head;
-    Node* slow = head;
-    Node* fast = head;
-    while(fast->next && fast->next->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-    return slow;
-}
+
