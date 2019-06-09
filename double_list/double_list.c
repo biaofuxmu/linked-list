06e5aab1d4e0f5adabf0c407fd473f3d16e59b97
hongknight <13750897616@163.com>
2019-06-10 01:08:25 +0800

    add notes of each function

--- double_list/double_list.c
+++ double_list/double_list.c
@@ -1,9 +1,9 @@
 #include "double_list.h"
 
-//创建结点
+//创建双向链表结点
 Node* create_node(int data)
 {
-    Node* node = (Node*)malloc(sizeof(Node));
+    Node* node = (Node*)malloc(sizeof(Node));//申请内存
     node->data = data;
     node->prev = NULL;
     node->next = NULL;
@@ -66,15 +66,20 @@ void push_head(DoubleList* list, int data)
 {
     Node* new_node = create_node(data);
 
+    //链表为空
     if(list->size == 0)
     {
+        //插入的结点既是头结点，也是尾结点
         list->head = new_node;
         list->tail = new_node;
     }
+    //链表不为空
     else
     {
+        //将新结点和链表通过前驱指针prev和后继指针next连接起来
         new_node->next = list->head;
         list->head->prev = new_node;
+        //将头结点改为新插入的结点
         list->head = new_node;
     }
     list->size++;
@@ -84,31 +89,40 @@ void push_head(DoubleList* list, int data)
 void push_tail(DoubleList* list, int data)
 {
     Node* new_node = create_node(data);
+
+    //链表为空
     if(list->size == 0)
     {
+        //插入的结点既是尾结点，也是头结点
         list->tail = new_node;
         list->head = new_node;
     }
+    //链表不为空
     else
     {
+        //将新结点和链表通过前驱指针prev和后继指针next连接起来
         list->tail->next = new_node;
         new_node->prev = list->tail;
+        //将尾结点改为新插入的结点
         list->tail = new_node;
     }
     list->size++;
     
 }
 
-//任意位置插入一个结点
+//任意位置插入结点
 bool insert_node(DoubleList* list, int index, int data)
 { 
+    //插入的位置超出链表的范围
     if(index <0 || index > list->size)
         return false;
+    //插入的位置如果等于0，等同于插入头结点
     if(index == 0)
     {
         push_head(list,data);
         return true;
     }
+    //插入的位置如果等于链表的长度，等同于插入尾结点
     else if(index == list->size)
     {
         push_tail(list,data);
@@ -116,16 +130,20 @@ bool insert_node(DoubleList* list, int index, int data)
     }
 
     Node* tmp = NULL;
+    //插入的位置如果小于等于中间位置，那插入的位置更靠近头结点
     if(index <= list->size / 2)
     {
+        //从头结点开始往后遍历到插入的前一个位置
         tmp = list->head;
         for(int i=0; i<index-1;i++)
         {
             tmp = tmp->next;
         }
     }
+    //插入的位置如果大于中间位置，那插入的位置更靠近尾结点
     else
     {
+        //从尾结点开始往前遍历到插入的前一个位置
         tmp = list->tail;
         for (int i = 0; i < list->size - index; ++i)
         {
@@ -150,16 +168,16 @@ void del_head(DoubleList* list)
         return;
     if(list->size == 1)
     {
-        free(list->head);
+        free(list->head);//释放内存
         init_list(list);
         return;
     }
 
-    Node* tmp = list->head;
+    Node* tmp = list->head;//保存头结点
     list->head = tmp->next;
     list->head->prev = NULL;
 
-    free(tmp);
+    free(tmp);//释放内存
     tmp = NULL;
     list->size--;
 }
@@ -171,16 +189,16 @@ void del_tail(DoubleList* list)
         return;
     if(list->size == 1)
     {
-        free(list->tail);
+        free(list->tail);//释放内存
         init_list(list);
         return;
     }
 
-    Node* tmp = list->tail;
+    Node* tmp = list->tail;//保存尾结点
     list->tail = tmp->prev;
     list->tail->next = NULL;
 
-    free(tmp);
+    free(tmp);//释放内存
     tmp = NULL;
     list->size--;
 }
@@ -188,13 +206,16 @@ void del_tail(DoubleList* list)
 //删除任意位置结点
 bool del_node(DoubleList* list, int index)
 {
+    //判断插入的位置是否超出链表的范围
     if(index < 0 || index > list->size)
-        return true;
+        return false;
+    //删除的位置如果等于0，等同于删除头结点
     if(index == 0)
     {
         del_head(list);
         return true;
     }
+    //删除的位置如果等于链表的长度，等同于删除尾结点
     if(index == list->size)
     {
         del_tail(list);
@@ -202,16 +223,21 @@ bool del_node(DoubleList* list, int index)
     }
 
     Node* tmp = NULL;
+
+    //删除的位置如果小于中间位置
     if(index < list->size/2)
     {
+        //从头结点开始往后遍历到删除的的结点
         tmp = list->head;
         for (int i = 0; i < index; ++i)
         {
             tmp = tmp->next;
         }
     }
+    //删除的位置如果大于等于中间位置
     else
     {
+        //从尾结点开始往前遍历到删除的结点
         tmp = list->tail;
         for (int i = 0; i < list->size - index - 1; ++i)
         {
@@ -221,29 +247,33 @@ bool del_node(DoubleList* list, int index)
 
     tmp->prev->next = tmp->next;
     tmp->next->prev = tmp->prev;
-    free(tmp);
+    free(tmp);//释放内存
     tmp = NULL;
     list->size--;
     return true;
 }
 
-
 //获取任意位置结点
 Node* get_node(DoubleList* list, int index)
 {
+    //判断插入的位置是否超出链表的范围
     if(index < 0 || index > list->size)
         return NULL;
     Node* node = NULL;
+    //目标结点的位置如果小于中间位置
     if(index < list->size / 2)
     {
+        //从头结点开始往后遍历到目标结点
         node = list->head;
         for(int i =0; i < index ; i++)
         {
             node = node->next;
         }
     }
+    //目标结点的位置如果大于等于中间位置
     else
     {
+        //从尾结点开始往前遍历到目标结点
         node = list->tail;
         for (int i = 0; i < list->size - index - 1; ++i)
         {
@@ -253,7 +283,7 @@ Node* get_node(DoubleList* list, int index)
     return node; 
 }
 
-//查找链表中指定的数据
+//获取链表中指定数据所在的第一个结点
 Node* find_data(DoubleList* list, int data)
 {
     if(list->size == 0)
@@ -280,7 +310,7 @@ bool modify_node(DoubleList* list, int index, int data)
     return false;
 }
 
-//修改链表中指定数据的值
+//修改链表中指定的第一个数据
 bool modify_data(DoubleList* list, int data, int val)
 {
     Node* node = find_data(list,data);
